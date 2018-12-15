16b2a62a3f17501701b63b51700242510324737d
usslss <ngfy123@163.com>
2018-12-15 14:28:25 +0800

    reconsturct the structure

--- pages/card/card.js
+++ pages/card/card.js
@@ -2,13 +2,14 @@ Page({
   onShareAppMessage() {
     return {
       title: '新增联系人',
-      path: 'page/API/pages/add-contact/add-contact'
+      path: 'page/API/pages/add-contact/add-contacts'
     }
   },
 
   submit(e) {
     const formData = e.detail.value
     wx.addPhoneContact({
+      firstName: this.data.firstName,
       ...formData,
       success() {
         wx.showToast({
